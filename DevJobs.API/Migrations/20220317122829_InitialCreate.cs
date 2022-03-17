d4831fa7de8102a2ca6bd6f9db11e55c6a439e40
Cesar Alves Pereira <cesar.pereira@vetta.digital>
2022-03-17 11:47:00 -0300

    Adicionado conexão ao banco de dados e Migrations

---
+++ DevJobs.API/Migrations/20220317122829_InitialCreate.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevJobs.API.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JobVacancies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Company = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsRemote = table.Column<bool>(type: "bit", nullable: false),
+                    SalaryRange = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobVacancies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JobApplication",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ApplicantEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ApplicantName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IdJobVacancy = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobApplication", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JobApplication_JobVacancies_IdJobVacancy",
+                        column: x => x.IdJobVacancy,
+                        principalTable: "JobVacancies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobApplication_IdJobVacancy",
+                table: "JobApplication",
+                column: "IdJobVacancy");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobApplication");
+
+            migrationBuilder.DropTable(
+                name: "JobVacancies");
+        }
+    }
+}
